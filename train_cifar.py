7e59d8d867edeeb76bb7f6585d2a8f3405519b20
Nick Sum <nicksum107@gmail.com>
2020-10-10 14:15:43 -0400

    bagnet

--- train_cifar.py
+++ train_cifar.py
@@ -56,11 +56,11 @@ transform_test = transforms.Compose([
 
 trainset = torchvision.datasets.CIFAR10(root='./data/cifar', train=True, download=True, transform=transform_train)
 
-trainloader = torch.utils.data.DataLoader(trainset, batch_size=64, shuffle=True, num_workers=2)
+trainloader = torch.utils.data.DataLoader(trainset, batch_size=32, shuffle=True, num_workers=2)
 
 testset = torchvision.datasets.CIFAR10(root='./data/cifar', train=False, download=True, transform=transform_test)
 
-testloader = torch.utils.data.DataLoader(testset, batch_size=64, shuffle=False, num_workers=2)
+testloader = torch.utils.data.DataLoader(testset, batch_size=32, shuffle=False, num_workers=2)
 
 classes = ('plane', 'car', 'bird', 'cat', 'deer', 'dog', 'frog', 'horse', 'ship', 'truck')
 
